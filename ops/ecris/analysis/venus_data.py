4e77449ff41bfc4205345eac2b1ce6845633012b
Jessica Rehak <rehak@lbl.gov>
2025-09-03 11:04:00 -0700

    added get venus data by data labels

--- ops/ecris/analysis/venus_data.py
+++ ops/ecris/analysis/venus_data.py
@@ -1,6 +1,7 @@
+import pandas as pd
 from pathlib import Path
 from datetime import datetime, timedelta
-from typing import List
+from typing import List, Optional
 
 _FILE_DATE_FORMAT = '%Y_%m_%d_%H_%M_%S'
 
@@ -21,3 +22,19 @@ def files_in_timeframe(files, start: datetime, stop: datetime) -> List[Path]:
         elif date > stop:
             break 
     return files_to_load
+
+def get_all_venus_data(path: Path, start: datetime, stop: datetime) -> pd.DataFrame:
+    files_to_load = files_in_timeframe(path.glob('*.parquet'), start, stop)
+    if not files_to_load:
+        raise ValueError('No parquet files found for provided time span')
+    return pd.concat([pd.read_parquet(f) for f in files_to_load])
+
+def get_venus_data(path: Path, data_label: str | List[str], start: datetime, stop: datetime) -> pd.DataFrame:
+    if isinstance(data_label, str):
+        data_labels = ['time', data_label]
+    else:
+        data_labels = ['time'] + data_label
+
+    all_data = get_all_venus_data(path, start, stop)
+    all_data['time'] = all_data['time'].apply(datetime.fromtimestamp)
+    return all_data[(start <= all_data['time']) & (all_data['time'] <= stop)].loc[:, data_labels]

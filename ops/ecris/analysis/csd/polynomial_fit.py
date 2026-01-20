29c196b39b85ba1469d9df3b989d11940f329e6e
Jessica Rehak <rehak@lbl.gov>
2026-01-20 10:18:03 -0800

    added optimization routine and shift of polynomial fitting

--- ops/ecris/analysis/csd/polynomial_fit.py
+++ ops/ecris/analysis/csd/polynomial_fit.py
@@ -5,6 +5,7 @@ from typing import List, Tuple, Optional
 import numpy as np
 from numpy.polynomial import Legendre
 import scipy.optimize as opt
+from scipy.signal import find_peaks
 
 from ops.ecris.analysis.model import CSD, Element
 from .m_over_q import estimate_m_over_q
@@ -21,8 +22,8 @@ def polynomial_fit_mq(
     csd: CSD,
     elements: List[Element],
     polynomial_order: int = 3,
-    linear_bounds: Optional[Tuple[float, float]] = None,
-    nonlinear_bounds: Tuple[float, float] = (-1, 1),
+    linear_bounds: Optional[Tuple[float, float]] = (0.95, 1.05),
+    nonlinear_bounds: Tuple[float, float] = (-1e-3, 1e-3),
     max_iterations: int = 1000,
     max_function_evaluations: Optional[int] = None,
     optimize_on_failure: bool = False,
@@ -31,8 +32,12 @@ def polynomial_fit_mq(
     if polynomial_order < 1:
         raise RuntimeError("Polynomial order must be at least linear")
     estimated_m_over_q = estimate_m_over_q(csd)
+    peaks, _ = find_peaks(csd.beam_current)
+    potential_h_lines = [estimated_m_over_q[int(p)] for p in peaks]
+    h_loc = estimated_m_over_q[np.argmin(np.abs([v - 1 for v in potential_h_lines]))]
+    estimated_m_over_q = estimated_m_over_q - h_loc
 
-    max_x = int(np.max(estimated_m_over_q)) + 1
+    max_x = int(np.max(estimated_m_over_q))
     x = np.linspace(0, max_x, 1000)
     _, unique_mask = np.unique(estimated_m_over_q, return_index=True)
     signal_x = estimated_m_over_q[unique_mask]
@@ -43,36 +48,34 @@ def polynomial_fit_mq(
     m_values = [e.atomic_mass for e in elements]
     template = np.zeros_like(mq)
     for m, q_max in zip(m_values, q_values):
-        for v in [m / q for q in range(1, q_max + 1) if m / q < max_x]:
+        for v in [m / q - h_loc for q in range(1, q_max + 1) if m / q < max_x]:
             i = np.argmin(np.abs(mq - v))
             template[i] = 100
 
     def residual(P):
-        polynomial = Legendre(P, window=[0, max_x], domain=[0, max_x])
+        polynomial = Legendre([0, *P], window=[0, max_x], domain=[0, max_x])
         x_mapping = polynomial(signal_x)
         if not np.all(np.diff(x_mapping) > 0):
             return 0
-        if not np.all(x_mapping > 0):
+        if not np.all(x_mapping > -1):
             return 0
         if x_mapping[-1] > 10:
             return 0
-        shifted_signal = np.interp(x, x_mapping, signal) / np.max(signal)
+        shifted_signal = np.interp(x, x_mapping, signal)
         return -float(np.correlate(template, shifted_signal)[0])
 
     sb = nonlinear_bounds
-    if linear_bounds is None:
-        lb = (9 / max_x, 11 / max_x)
-    else:
-        lb = linear_bounds
-    bounds = [sb, lb] + [sb] * (polynomial_order - 2)
+    lb = linear_bounds
+    bounds = [lb] + [sb] * (polynomial_order - 2)
     sol = opt.direct(
         residual,
         bounds,
         maxfun=max_function_evaluations,
         maxiter=max_iterations,
         locally_biased=False,
+        vol_tol=1e-16 / (10 * (polynomial_order - 2)),
     )
     if always_optimize or (optimize_on_failure and not sol.success):
         sol = opt.minimize(residual, sol.x, bounds=bounds, method="Nelder-Mead")
-    poly = Legendre(sol.x)
-    return poly(estimated_m_over_q), sol
+    poly = Legendre([0, *sol.x])
+    return poly(estimated_m_over_q) + h_loc, sol
